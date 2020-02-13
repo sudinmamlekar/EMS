c8795685d1ac3031f2e6ebcab80bb8e94382bc6f
sudinmamlekar <57784246+sudinmamlekar@users.noreply.github.com>
2020-02-13 13:27:29 +0530

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.html">Employee Management System</a>
+    </div>
+    <ul class="nav navbar-nav">
+     
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="elogin.php"><span class="glyphicon glyphicon-user"></span> Employee Login</a></li>
+      <li><a href="alogin.php"><span class="glyphicon glyphicon-log-in"></span> Admin Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+</body>
+</html>
